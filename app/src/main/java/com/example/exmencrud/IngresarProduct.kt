b9fe4baa8c463b0f2b2614dc9a1860bde468fa6c
Darwin Chamba <darwinchambaa@gmail.com>
2025-06-25 21:45:23 -0500

    modificando actividad Ingresar Producto-cambio local

--- app/src/main/java/com/example/exmencrud/IngresarProduct.kt
+++ app/src/main/java/com/example/exmencrud/IngresarProduct.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.exmencrud.databinding.ActivityIngresarProductBinding
 
+
 class IngresarProduct : AppCompatActivity() {
     private lateinit var binding: ActivityIngresarProductBinding
 
@@ -14,33 +15,45 @@ class IngresarProduct : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnGuardar.setOnClickListener {
-            if (binding.nameProduct.text.isNullOrBlank() ||
-                binding.descriptionProduct.text.isNullOrBlank() ||
-                binding.priceProduct.text.isNullOrBlank() ||
-                binding.stockProduct.text.isNullOrBlank() ||
-                binding.categoryProduct.text.isNullOrBlank()
-            ) {
-                Toast.makeText(this, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
-            val price = binding.priceProduct.text.toString().toDoubleOrNull()
-            val stock = binding.stockProduct.text.toString().toIntOrNull()
-
-            if (price == null || stock == null) {
-                Toast.makeText(this, "Precio o stock inválido", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
-            val producto = Producto(
-                name = binding.nameProduct.text.toString().trim(),
-                description = binding.descriptionProduct.text.toString().trim(),
-                price = price,
-                stock = stock,
-                category = binding.categoryProduct.text.toString().trim()
-            )
-
-            Toast.makeText(this, "Producto ingresado:\n$producto", Toast.LENGTH_LONG).show()
+            guardarProducto()
         }
     }
+    private fun guardarProducto(){
+
+
+
+        val price = binding.priceProduct.text.toString().toDoubleOrNull()
+        val stock = binding.stockProduct.text.toString().toIntOrNull()
+
+        if (price == null || stock == null) {
+            Toast.makeText(this, "Precio o stock inválido", Toast.LENGTH_SHORT).show()
+
+        }
+
+        val producto = price?.let {
+            stock?.let { it1 ->
+                Producto(
+                    name = binding.nameProduct.text.toString().trim(),
+                    description = binding.descriptionProduct.text.toString().trim(),
+                    price = it,
+                    stock = it1,
+                    category = binding.categoryProduct.text.toString().trim()
+                )
+            }
+        }
+
+        Toast.makeText(this, "Producto ingresado:\n$producto", Toast.LENGTH_LONG).show()
+    }
+    private fun validarCampos():Producto?{
+        if (binding.nameProduct.text.isNullOrBlank() ||
+            binding.descriptionProduct.text.isNullOrBlank() ||
+            binding.priceProduct.text.isNullOrBlank() ||
+            binding.stockProduct.text.isNullOrBlank() ||
+            binding.categoryProduct.text.isNullOrBlank()
+        ) {}
+            //Toast.makeText(this, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show()
+
+        return null
+    }
+
 }
